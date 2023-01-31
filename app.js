b73de83c2be33d110e23bdb5c8d7be8ac47dbf5f
DenizAlakavuklar <deniz.kovanci@gmail.com>
2023-01-31 12:04:16 +0100

    Nav bar and about me basics are created.

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-const express = require('express');
-const { isAbsolute } = require('path');
-const app = express();
-const port = 3000;
+const express = require('express')
+const app = express()
+const port = 3000
 
 app.use(express.static('public'))
 
-app.get('/home', (req, res) => {
+app.get('/', (req, res) => {
   console.log(req.url)
   res.sendFile(__dirname + '/views/index.html')
 })
 
-app.get('/about', (req, res) => {
+app.get('/about', (req, res,) => {
     console.log(req.url)
     res.sendFile(__dirname + '/views/about.html')
+    //res.send("It is about Doruk Alakavuklar!")
   })
 
 app.get('/works', (req, res) => {
@@ -28,3 +28,4 @@ app.get('/images', (req, res) => {
 app.listen(3000, () => {
   console.log(`now listening`)
 })
+
